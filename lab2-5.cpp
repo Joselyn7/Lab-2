88e376ddbf7625cf8aa90a3100f0d757c0f6dd46
Joselyn7 <64213758+Joselyn7@users.noreply.github.com>
2020-05-17 21:26:05 -0500

    Update lab2-5.cpp

--- lab2-5.cpp
+++ lab2-5.cpp
@@ -1,26 +1,24 @@
 //PROBLEMA 5
 #include <iostream>
-#include <cstring>
 using namespace std;
-void con(char *b,int t2,char *a,int t,int l)
+void con(char b[],int t2,int t,char a[])
 {
 	if(t2>0){
-		*(b+t2+t-1)=*(b+t2-1);
-		return con(b,--t2,a,t,l);
+		b[t2+t-1]=b[t2-1];
+		return con(b,--t2,t,a);
 	}
-	else if(t>0){
-		*(b+t-1)=*(a+t-1);
-		return con(b,t2,a,--t,l);
-	} 
+	for(int i=0;i<t;i++)
+		b[i]=a[i];
+}
+void mostrar(char b[],int l)
+{
 	for(int i=0;i<l;i++)
-		cout<<*(b+i);
+		cout<<b[i];
 }
 int main()
 {
-	char *a= new char[5];
-	char *b= new char[10];
-	cout<<"1:"; gets(a);
-	cout<<"2:"; gets(b);
-	int t=strlen(a) , t2=strlen(b), l=t+t2;
-	con(b,t2,a,t,l);
+	char b[]={"flores"};
+	char a[]={"gato"};
+	con(b,6,4,a);
+	mostrar(b,10); //10 es el tamaño b + tamaño a
 }
